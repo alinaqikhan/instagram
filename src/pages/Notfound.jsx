d6621b71055f6ca536fc3b0f27296a857130d5ab
Ali Naqi Khan <alinaqikofficial@gmail.com>
2021-09-24 20:07:22 +0500

    Updated Not found page
    Added header component in the Not found page

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import Header from '../components/Header';
 
-const Notfound = () => {
-    useEffect(() => {
-        document.title = 'Not Found - Instagram';
-    }, [])
-    return (
-        <div className="app bg-gray-background">
-            <div className="mx-auto max-w-screen-lg">
-                <p className="text-center text-2xl">
-                    Not Found!
-                </p>
-            </div>
-        </div>
-    )
+export default function NotFound() {
+  useEffect(() => {
+    document.title = 'Not Found - Instagram';
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
 }
-
-export default Notfound
